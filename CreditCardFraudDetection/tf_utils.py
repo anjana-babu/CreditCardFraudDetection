43c33a6e13c5fe0befcc5e2333282fb3803f2c6d
anjana-babu <anjana.babu@gmail.com>
2020-02-16 19:13:48 +0100

    Add files via upload
    Trained model

--- CreditCardFraudDetection/tf_utils.py
+++ CreditCardFraudDetection/tf_utils.py
@@ -67,12 +67,12 @@ def convert_to_one_hot(Y, C):
 
 def predict(X, parameters):
     
-    W1 = tf.convert_to_tensor(parameters["W1"])
-    b1 = tf.convert_to_tensor(parameters["b1"])
-    W2 = tf.convert_to_tensor(parameters["W2"])
-    b2 = tf.convert_to_tensor(parameters["b2"])
-    W3 = tf.convert_to_tensor(parameters["W3"])
-    b3 = tf.convert_to_tensor(parameters["b3"])
+    W1 = tf.convert_to_tensor(value=parameters["W1"])
+    b1 = tf.convert_to_tensor(value=parameters["b1"])
+    W2 = tf.convert_to_tensor(value=parameters["W2"])
+    b2 = tf.convert_to_tensor(value=parameters["b2"])
+    W3 = tf.convert_to_tensor(value=parameters["W3"])
+    b3 = tf.convert_to_tensor(value=parameters["b3"])
     
     params = {"W1": W1,
               "b1": b1,
@@ -81,12 +81,12 @@ def predict(X, parameters):
               "W3": W3,
               "b3": b3}
     
-    x = tf.placeholder("float", [12288, 6])
+    x = tf.compat.v1.placeholder("float", [12288, 30])
     
     z3 = forward_propagation_for_predict(x, params)
-    p = tf.argmax(z3)
+    p = tf.argmax(input=z3)
     
-    sess = tf.Session()
+    sess = tf.compat.v1.Session()
     prediction = sess.run(p, feed_dict = {x: X})
         
     return prediction
